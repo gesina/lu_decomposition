5c4d9484686f78d03f1e47106aaa8b7f8f944457
Gesina Schwalbe <gesina.schwalbe@googlemail.com>
2014-11-09 17:51:49 +0100

    completely working equation solving

--- solve_equation.h
+++ solve_equation.h
@@ -31,5 +31,24 @@
 
 void solve_equation(double** R, double* z, double* x, int dim)
 {
+  // got through the entries in x and set
+  // x_{k} = [ z_{k}^{(k-1)} - ( \sum_{j=k+1}^{n} r_{kj}^{(k-1)}*x_{j} ) ]
+  
+  // for k=n, ..., 1
+  for (int k=dim-1; k>=0; k--)
+    {
+      // x_{k} = z_{k}
+      *(x+k) = *(z+k);
 
+      // for j=k+1, ..., n
+      for (int j=k+1; j<dim; j++)
+	{
+	  // x_{k} = x_{k}-r_{kj}x_{j}
+	  *(x+k) = *(x+k) - (*(*(R+k)+j)) * (*(x+j));
+	}
+
+      // x_{k} = x_{k}/r_{kk}
+      *(x+k) = (*(x+k)) /  (*(*(R+k)+k));
+      
+    }
 }
